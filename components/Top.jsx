29c82a1ea143e8cc94be844ffefd5cb3e8075810
gentlemankz <shynggys.zhakenuly@gmail.com>
2024-11-13 22:05:25 +0500

    🚑 fixed bug

--- components/Top.jsx
+++ components/Top.jsx
@@ -3,16 +3,13 @@ import InfoTop from "./InfoTop";
 import Text from "./Text";
 
 export default function Top({ article }) {
-    const title = typeof article.title === "string" ? article.title : JSON.stringify(article.title);
-
     return (
         <div className="flex flex-col w-[693px] gap-12 p-0 mr-16">
             <InfoTop byline={article.byline} section={article.section} published_date={article.published_date} />
-            <Text title={title} abstract={article.abstract} />
+            <Text title={article.title} abstract={article.abstract} />
         </div>
     );
 }
-
 /* 
 {
         "section": "business",
